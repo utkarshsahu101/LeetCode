ddb0efe010dfc2bff81e8e6f198f677c9646323a
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-04-15 20:21:40 +0530

    7.541. Reverse String II

---
+++ Leetcode-Problems/7.541. Reverse String II/Solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+  s = s.split("");
+
+  for (let x = 0; x < s.length; x = x + 2 * k) {
+    let n = k;
+    let mid = x + Math.floor(n / 2);
+    for (let i = x; i < mid; i++) {
+      // due to fixed k -> O(1)
+      let temp = s[i];
+      s[i] = s[n - 1 - i];
+      s[n - 1 - i] = temp;
+    }
+  }
+
+  return s.join("");
+};
+
+// TC - O(n)
