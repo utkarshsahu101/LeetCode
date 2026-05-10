f6081d7998a14068463f6a8961dca325a7121295
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-05-10 19:28:40 +0530

    14.225. Implement Stack using Queues - using 1 queue

--- Leetcode-Problems/14.225. Implement Stack using Queues/Solution.js
+++ Leetcode-Problems/14.225. Implement Stack using Queues/Solution.js
@@ -63,3 +63,58 @@ MyStack.prototype.empty = function () {
  * var param_3 = obj.top()
  * var param_4 = obj.empty()
  */
+
+// Using 1 queue
+
+var MyStack = function () {
+  this.q = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.q.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  let n = this.q.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift());
+  }
+  let lastEl = this.q.shift();
+  return lastEl;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  let n = this.q.length;
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift());
+  }
+  let lastEl = this.q.shift();
+  this.q.push(lastEl);
+  return lastEl;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.q.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
