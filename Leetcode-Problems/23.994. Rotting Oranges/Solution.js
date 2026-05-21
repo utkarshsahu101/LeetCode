ecd6ed3af2273ef3dd81f3f3ca63b82654bb3237
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-05-21 20:14:04 +0530

    23.994. Rotting Oranges

---
+++ Leetcode-Problems/23.994. Rotting Oranges/Solution.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function (grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+
+  const queue = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      }
+    }
+  }
+
+  let maxMinutes = 0;
+
+  while (queue.length) {
+    let [x, y, level] = queue.shift();
+    if (x > 0 && grid[x - 1][y] === 1) {
+      grid[x - 1][y] = 2;
+      queue.push([x - 1, y, level + 1]);
+    }
+    if (x < m - 1 && grid[x + 1][y] === 1) {
+      grid[x + 1][y] = 2;
+      queue.push([x + 1, y, level + 1]);
+    }
+    if (y > 0 && grid[x][y - 1] === 1) {
+      grid[x][y - 1] = 2;
+      queue.push([x, y - 1, level + 1]);
+    }
+    if (y < n - 1 && grid[x][y + 1] === 1) {
+      grid[x][y + 1] = 2;
+      queue.push([x, y + 1, level + 1]);
+    }
+    maxMinutes = Math.max(maxMinutes, level);
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        return -1;
+      }
+    }
+  }
+
+  return maxMinutes;
+};
+
+// TC - O(m * n) for iterating through the grid to find the rotten oranges and then iterating through the queue to rot the fresh oranges and then iterating through the grid again to check if there are any fresh oranges left
+// SC - O(m * n) for the queue to store the rotten oranges
