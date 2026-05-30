a50dcd688c242c33e07371432875f713ce46d29b
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-05-31 03:55:33 +0530

    34.658. Find K Closest Elements

---
+++ Leetcode-Problems/34.658. Find K Closest Elements/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (arr[m + k] - x < x - arr[m]) {
+      l = m + 1;
+    } else {
+      r = m;
+    }
+  }
+  return arr.slice(l, l + k);
+};
