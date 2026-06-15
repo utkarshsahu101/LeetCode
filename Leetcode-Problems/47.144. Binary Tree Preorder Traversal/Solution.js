65fa0cdfb9a9f0611e50435050e5382a9c588cd5
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-06-15 22:34:05 +0530

    47.144. Binary Tree Preorder Traversal - 2nd approach

--- Leetcode-Problems/47.144. Binary Tree Preorder Traversal/Solution.js
+++ Leetcode-Problems/47.144. Binary Tree Preorder Traversal/Solution.js
@@ -24,3 +24,29 @@ var preorderTraversal = function (root) {
 };
 // TC - O(n) for traversing all the nodes in the binary tree
 // SC - O(n) for storing the answer in the array and O(h) for the recursive stack space where h is the height of the binary tree
+
+// 2nd approach - using stack to traverse the binary tree iteratively and storing the answer in the array
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (!root) return [];
+  let ans = [];
+  let stack = [root];
+  while (stack.length) {
+    let curr = stack.pop();
+    ans.push(curr.val);
+    curr?.right && stack.push(curr.right);
+    curr?.left && stack.push(curr.left);
+  }
+  return ans;
+};
