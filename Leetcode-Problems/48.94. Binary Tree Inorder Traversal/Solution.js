051861fe98bb5ddb23afdc66176d1d98ed6eb2c6
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-06-20 23:50:02 +0530

    48.94. Binary Tree Inorder Traversal - 2nd approach iterative way

--- Leetcode-Problems/48.94. Binary Tree Inorder Traversal/Solution.js
+++ Leetcode-Problems/48.94. Binary Tree Inorder Traversal/Solution.js
@@ -21,3 +21,35 @@ var inorderTraversal = function (root) {
   traversal(root);
   return ans;
 };
+
+// 2nd approach - using stack to traverse the binary tree iteratively and storing the answer in the array
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let curr = root;
+  let stack = [];
+  let ans = [];
+  console.log(curr);
+  while (stack.length || curr) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    ans.push(curr.val);
+    curr = curr.right;
+  }
+  return ans;
+};
+// TC - O(n) for traversing all the nodes in the binary tree
+// SC - O(n) for storing the answer in the array and O(h) for the stack space where h is the height of the binary tree
