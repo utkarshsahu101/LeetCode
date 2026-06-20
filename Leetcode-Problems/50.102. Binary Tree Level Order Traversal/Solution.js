6409ade499293850efbe84235d070d334524b199
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-06-20 23:53:09 +0530

    50.102. Binary Tree Level Order Traversal

---
+++ Leetcode-Problems/50.102. Binary Tree Level Order Traversal/Solution.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) return [];
+  let queue = [root];
+  let ans = [];
+  while (queue.length) {
+    let levelOrderArr = [];
+    let levelOrderArrSize = queue.length;
+    for (let i = 0; i < levelOrderArrSize; i++) {
+      let curr = queue.shift();
+      curr.left && queue.push(curr.left);
+      curr.right && queue.push(curr.right);
+      levelOrderArr.push(curr.val);
+    }
+    ans.push(levelOrderArr);
+  }
+  return ans;
+};
+// TC - O(n) for traversing all the nodes in the binary tree
+// SC - O(n) for storing the answer in the array and O(h) for the queue space where h is the height of the binary tree
