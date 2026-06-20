f399e075871e4d2ae6b35b362b97b019cf6da216
Utkarsh Sahu <utkarshsahugithub101@gmail.com>
2026-06-20 23:51:16 +0530

    49.145. Binary Tree Postorder Traversal - 3rd approach - iterative using 1 stack only

--- Leetcode-Problems/49.145. Binary Tree Postorder Traversal/Solution.js
+++ Leetcode-Problems/49.145. Binary Tree Postorder Traversal/Solution.js
@@ -52,3 +52,37 @@ var postorderTraversal = function (root) {
 };
 // TC - O(n) for traversing all the nodes in the binary tree
 // SC - O(n) for storing the answer in the array and O(h) for the stack space where h is the height of the binary tree
+
+// 3rd approach - using 1 stack
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  let stack = [];
+  let ans = [];
+  let curr = root;
+  let lastVisitedNode = null;
+  while (stack.length || curr) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    let peekNode = stack[stack.length - 1];
+    if (peekNode.right && peekNode.right != lastVisitedNode) {
+      curr = peekNode.right;
+    } else {
+      ans.push(peekNode.val);
+      lastVisitedNode = stack.pop();
+    }
+  }
+  return ans;
+};
